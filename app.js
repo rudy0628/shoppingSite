5114c33825fae16f18f1cbd279befae77f05c2e9
Rudy yeh <s3352250zz@gamil.com>
2021-09-09 12:59:49 +0800

    add start script

--- app.js
+++ app.js
@@ -145,6 +145,7 @@ app.use((err, req, res, next) => {
 });
 
 //listen port
-app.listen(3000, () => {
-	console.log('listen to port 3000!');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+	console.log(`listen to port ${port}!`);
 });
